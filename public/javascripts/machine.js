7ad0016136a692ac6473daf6c0b2f0430b96c58b
Aniket Survase <aniketsr@thoughtworks.com>
2014-11-06 13:58:42 +0530

    Ani/Digi - fixing input validations

--- public/javascripts/machine.js
+++ public/javascripts/machine.js
@@ -13,9 +13,12 @@ var Machine = function(startState, finalStates, all) {
 	self.isInputAccepted = function(text) {
 		var result = true;
 
+		if(!self.startState) return false;
+		
 		text.split('').forEach(function(input) {
 			result  = self.acceptInput(input);
 		});
+
 		if(!result) return result;
 		result = (finalStates.indexOf(self.current) != -1);
 		self.current = self.startState;
